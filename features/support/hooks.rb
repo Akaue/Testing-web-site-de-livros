43dae5e160eca76c463d78b5b140eb134c2ec4c6
Akauê <sk2_skt@hotmail.com>
2020-10-28 09:29:27 -0300

    Alterações realizadas

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -8,13 +8,13 @@ Before do
 end
 
 
-After do |scenario|
-    nome_cenario = scenario.name.gsub(/[^A-Za-z0-9]/,' ')
-    screenshot = "log/screenshots/#{nome_cenario}.png"
-    page.save_screenshot(screenshot)
-    attach(screenshot,'image/png')
+# After do |scenario|
+#     nome_cenario = scenario.name.gsub(/[^A-Za-z0-9]/,' ')
+#     screenshot = "log/screenshots/#{nome_cenario}.png"
+#     page.save_screenshot(screenshot)
+#     attach(screenshot,'image/png')
 
-end
+# end
 
 # at_exit do   
 #   ReportBuilder.configure do |config|
