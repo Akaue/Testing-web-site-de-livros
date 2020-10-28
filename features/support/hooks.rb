10d77960b58264be5d3bd7d0cdc50f15c357de14
Akauê <sk2_skt@hotmail.com>
2020-10-28 09:59:40 -0300

    Enviando alteraçõe

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -4,7 +4,7 @@ Before do
     @cadastro = CadastroPage.new
     @login = LoginPage.new
     @shop = ShopPage.new
-    page.current_window.resize_to(1700, 600)      
+    page.current_window.resize_to(1920, 1080)    
 end
 
 
