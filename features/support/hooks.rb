6e017c1c95915671e516dfc4daa2d83aeb58331e
Akauê <sk2_skt@hotmail.com>
2020-10-29 11:31:36 -0300

    Alterando hooks

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,5 +1,3 @@
-# require 'report_builder'
-
 Before do
     @cadastro = CadastroPage.new
     @login = LoginPage.new
@@ -8,24 +6,13 @@ Before do
 end
 
 
-# After do |scenario|
-#     nome_cenario = scenario.name.gsub(/[^A-Za-z0-9]/,' ')
-#     screenshot = "log/screenshots/#{nome_cenario}.png"
-#     page.save_screenshot(screenshot)
-#     attach(screenshot,'image/png')
+After do |scenario|
+    nome_cenario = scenario.name.gsub(/[^A-Za-z0-9]/,' ')
+    screenshot = "log/screenshots/#{nome_cenario}.png"
+    page.save_screenshot(screenshot)
+    attach(screenshot,'image/png')
 
-# end
+end
 
-# at_exit do   
-#   ReportBuilder.configure do |config|
-#      config.input_path = 'log/report.json'
-#      config.report_path = 'log/report'
-#      config.report_types = [:retry, :html]
-#      config.report_title = 'Evidências dos testes'
-#      config.compress_image = true
-#      config.additional_info = {"app" => "web livros","Data de execução" => "20/10/2020"}
-#    end
-#  ReportBuilder.build_report
-# end
 
 
