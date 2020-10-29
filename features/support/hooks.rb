39cc9a32f611346b259d96f7ddac2f691eb2cfdd
Akauê <sk2_skt@hotmail.com>
2020-10-29 11:50:33 -0300

    Adicionando alterações no hook

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -6,13 +6,6 @@ Before do
 end
 
 
-After do |scenario|
-    nome_cenario = scenario.name.gsub(/[^A-Za-z0-9]/,' ')
-    screenshot = "log/screenshots/#{nome_cenario}.png"
-    page.save_screenshot(screenshot)
-    attach(screenshot,'image/png')
-
-end
 
 
 
